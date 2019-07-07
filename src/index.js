b5b4ea86a1c343a02b8883a8e97b5639941f8fb8
Taro <publictaro@gmail.com>
2019-07-07 18:38:12 +0900

    fix text length

--- src/index.js
+++ src/index.js
@@ -89,8 +89,8 @@ class Main extends React.Component{
                         <Col md="4" className="avator"><img src="https://pbs.twimg.com/profile_banners/2265077570/1537969176/1500x500" alt="太郎の自撮り" className="mypic"/></Col>
                         <Col md="8" className="introduction-wrapper">
                           <div　className="introduction-text">
-                            <p>私は社内のシステムエンジニアです。ユーザー要件の取り纏めや拠点間の調整を中心に行なっています。</p>
-                            <p>入社以来開発業務は行なっていませんが、趣味で帰宅後や休日を使ってWeb技術を中心に勉強しています。</p>
+                            <p>社内向けのSEをしています。ユーザー要件の取り纏めが主な業務です。</p>
+                            <p>入社以来開発業務は行なっていませんが、帰宅後や休日を使ってWeb技術を中心に勉強を続けています。</p>
                           </div>
                         </Col>
                       </Row>
@@ -156,6 +156,7 @@ class Main extends React.Component{
         
 }
 class Footer extends React.Component{
+
     render(){
         return(
             <footer>
