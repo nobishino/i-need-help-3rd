697d301bf96eb6cf779735c63dd26d4011099aad
Taro <privatetaro@gmail.com>
2019-07-17 07:47:57 +0900

    add modal image #6

--- src/project.js
+++ src/project.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Row,Col} from 'reactstrap';
 import {Card,CardImg,CardTitle,CardBody} from 'reactstrap';
+import {Modal,ModalHeader,ModalBody} from 'reactstrap';
 import aws from './MyAWSNetwork.png';
 import portfolio from './MyPortfolio.png';
 
@@ -9,6 +10,23 @@ import './updatebs4.css';
 
 
 export default class Project extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      modal:false,
+      img:aws,
+      title:""
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+  toggle(image,title){
+      this.setState(prevState => ({
+          modal: !prevState.modal,
+          img:image,
+          title:title
+        }));
+  }
+  
 
     render(){
         return(
@@ -18,7 +36,7 @@ export default class Project extends React.Component{
               <Row>
                 <Col md="6">
                   <Card className ="productcard">
-                    <CardImg top src={aws} alt="myawsnetwork" />
+                    <a  onClick={this.toggle.bind(this,aws,"AWSネットワーク構成")}><CardImg top src={aws} alt="myawsnetwork" /></a>
                     <CardTitle>My AWS Network</CardTitle>
                     <CardBody>WordPressの公開を目的にAWSのネットワーク構成を検討・構築しました。Webサーバーのみインターネットに公開され、
                     DBサーバーは外部から参照できないようになっています。今後はELBを導入し冗長性をあげることが課題です。</CardBody>
@@ -26,11 +44,15 @@ export default class Project extends React.Component{
                 </Col>
                 <Col md="6">
                   <Card className="productcard">
-                    <CardImg top src={portfolio} alt="myawsnetwork" />
+                    <a onClick={this.toggle.bind(this,portfolio,"ポートフォリオ")}><CardImg top src={portfolio} alt="myawsnetwork" /></a>
                     <CardTitle>My Portfolio</CardTitle>
                     <CardBody>Reactの学習を目的に、デザイン・コンポーネントの構成を検討し、スクラッチしました。コンポーネントの再利用性を理解するには十分でしたがstate管理、外部APIとの連携を次の課題として取り入れたいと考えています。</CardBody>
                   </Card>
                 </Col>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>{this.state.title}</ModalHeader>
+                    <ModalBody><img className="modalimg" src={this.state.img}/></ModalBody>
+                  </Modal>
               </Row>
             </Card>
           </Col>
