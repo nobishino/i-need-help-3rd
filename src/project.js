e5177a81af1814e4b4de9db7f01f86a4d8f7305a
Taro <privatetaro@gmail.com>
2019-07-17 22:41:34 +0900

    #11 modify img size title size

--- src/project.js
+++ src/project.js
@@ -37,21 +37,22 @@ export default class Project extends React.Component{
                 <Col md="6">
                   <Card className ="productcard">
                     <a  onClick={this.toggle.bind(this,aws,"AWSネットワーク構成")}><CardImg top src={aws} alt="myawsnetwork" /></a>
-                    <CardTitle>My AWS Network</CardTitle>
-                    <CardBody>WordPressの公開を目的にAWSのネットワーク構成を検討・構築しました。Webサーバーのみインターネットに公開され、
-                    DBサーバーは外部から参照できないようになっています。今後はELBを導入し冗長性をあげることが課題です。</CardBody>
+                    <CardTitle><h5>My AWS Network</h5></CardTitle>
+                    <CardBody className="description">WordPressの公開を目的にAWSのネットワーク構成を検討・構築しました。Webサーバーのみインターネットに公開され、
+                    DBサーバーは外部から参照できないようになっています。今後はELBを導入し冗長性をあげたいと考えています。</CardBody>
                   </Card>
                 </Col>
                 <Col md="6">
-                  <Card className="productcard">
-                    <a onClick={this.toggle.bind(this,portfolio,"ポートフォリオ")}><CardImg top src={portfolio} alt="myawsnetwork" /></a>
-                    <CardTitle>My Portfolio</CardTitle>
-                    <CardBody>Reactの学習を目的に、デザイン・コンポーネントの構成を検討し、スクラッチしました。コンポーネントの再利用性を理解するには十分でしたがstate管理、外部APIとの連携を次の課題として取り入れたいと考えています。</CardBody>
+                  <Card className="productcard" >
+                    <a onClick={this.toggle.bind(this,portfolio,"ポートフォリオ")}><CardImg  top src={portfolio} alt="myawsnetwork"  /></a>
+                    <CardTitle><h5>My Portfolio</h5></CardTitle>
+                    <CardBody className="description">Reactの学習を目的に、デザイン・コンポーネントの構成を検討し、スクラッチしました。
+                    コンポーネントの再利用性を理解するには十分でしたが、今後はstate管理、外部APIとの連携を取り入れたいと考えています。</CardBody>
                   </Card>
                 </Col>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} >
                     <ModalHeader toggle={this.toggle}>{this.state.title}</ModalHeader>
-                    <ModalBody><img className="modalimg" src={this.state.img}/></ModalBody>
+                    <ModalBody><img className="modalimg"  src={this.state.img}/></ModalBody>
                   </Modal>
               </Row>
             </Card>
